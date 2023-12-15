b4169cae6e8b2d6030f0552b169618d79f37f7f7
Albertogm98 <albertogm.personal@gmail.com>
2023-12-15 14:44:14 +0100

    Correción

--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,23 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    // Configuración de destinatario y encabezados
+    $to = "albertogm.personal@gmail.com";  // Reemplaza con tu dirección de correo
+    $headers = "From: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    // Construye el cuerpo del correo
+    $body = "Nombre: $name <br>";
+    $body .= "Email: $email <br>";
+    $body .= "Asunto: $subject <br>";
+    $body .= "Mensaje: $message";
+
+    // Envía el correo
+    mail($to, $subject, $body, $headers);
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "albertogm.personal@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "Ha recibido un nuevo mensaje desde el formulario de contacto de su sitio web.\n\n"."Aquí los detalles:\n\nNombre: $name\n\n\nE-mail: $email\n\nAsunto: $m_subject\n\nMenesaje: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
 ?>
